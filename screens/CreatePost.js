4ee6ebfd99375c13ba8e8a3e39e3245c26f032d9
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-01-07 16:28:51 +0530

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+import {Text,View} from 'react-native'
+export default class CreatePost extends Component{
+  render(){
+    return(
+      <View><Text>create Post</Text></View>
+    )
+  }
+}
